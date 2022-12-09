9d45d2cfc4c5289226d59d6e83fed2c5d4e9e997
Sara Pablo <lunielledev@gmail.com>
2022-12-10 00:43:19 +0100

    Builtin datatypes

---
+++ src/main/kotlin/com/saraflower/ktdemo/datatypes/datatypes.kt
@@ -0,0 +1,25 @@
+package com.saraflower.ktdemo.datatypes
+
+fun main() {
+    val int = 10
+    var long = 22L
+
+    //long = int will not work automatically, there is no number widening
+    long = int.toLong()
+
+    val byte: Byte = 111
+    val short: Short = byte.toShort() // same here
+
+    val float = 25.25f
+    val double: Double = float.toDouble()
+
+    val char = 'b'
+    //This won't work, chars are treated as chars
+    //val char2: Char = 65
+    val myCharInt = 65
+    val converted: Char = myCharInt.toChar() //same thing
+
+    //Will not work either
+    //val notBoolean: Boolean = 1
+    val boolean = true
+}
